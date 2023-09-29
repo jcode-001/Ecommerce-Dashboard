a796d84928a7193689a249d4a12d120132cd9d5f
Olaleye Tobi <olaleyetobi932@gmail.com>
2023-09-29 18:25:05 +0100

    automatically change to dark theme when the time is above 18:00

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
 "use strict";
-
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
 const themeToggler = document.querySelector(".theme-toggler");
+const currentDate = new Date();
+const currentHour = currentDate.getHours();
 
 // display the side bar when the hamburger icon is clicked
 const displayMenu = () => {
   sideMenu.style.display = "block";
 };
-menuBtn.addEventListener("click", displayMenu);
 
 // hides the side bar when the hamburger icon is clicked
 const hideMenu = () => {
   sideMenu.style.display = "none";
 };
+menuBtn.addEventListener("click", displayMenu);
 closeBtn.addEventListener("click", hideMenu);
 
+// change dashboard theme to light/dark mode
 const changeTheme = () => {
   document.body.classList.toggle("dark-theme-variables");
-
   themeToggler.querySelector("span").classList.toggle("active");
 };
 themeToggler.addEventListener("click", changeTheme);
+if (currentHour > 18) {
+  changeTheme()
+}
 
+
+// fill in recent orders with data generated from the order.js file
 orders.forEach((order) => {
   const tr = document.createElement("tr");
 
